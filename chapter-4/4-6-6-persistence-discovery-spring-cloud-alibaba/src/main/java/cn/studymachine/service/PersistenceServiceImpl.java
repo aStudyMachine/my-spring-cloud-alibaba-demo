37195582a0d222891dfbee08c3a331e716a3dbcf
wukun <studymachine@foxmail.com>
2022-01-10 23:17:47 +0800

    4-6-6 Nacos 实例持久化

---
+++ chapter-4/4-6-6-persistence-discovery-spring-cloud-alibaba/src/main/java/cn/studymachine/service/PersistenceServiceImpl.java
@@ -0,0 +1,11 @@
+package cn.studymachine.service;
+
+import org.apache.dubbo.config.annotation.DubboService;
+
+@DubboService(group = "persistence-discovery-spring-cloud-alibaba",version = "1.0.0")
+public class PersistenceServiceImpl implements PersistenceService {
+    @Override
+    public String persistenceTest() {
+        return "persistence success!";
+    }
+}
