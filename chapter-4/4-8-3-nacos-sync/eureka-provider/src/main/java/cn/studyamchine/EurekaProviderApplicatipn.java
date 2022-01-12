2c55deb16359877578151c9bc08e07c5dc4b114b
wukun <studymachine@foxmail.com>
2022-01-12 22:55:16 +0800

    4-8-3 eureka 同步到 nacos 实例 (待完成)

---
+++ chapter-4/4-8-3-nacos-sync/eureka-provider/src/main/java/cn/studyamchine/EurekaProviderApplicatipn.java
@@ -0,0 +1,13 @@
+package cn.studyamchine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class EurekaProviderApplicatipn {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaProviderApplicatipn.class, args);
+    }
+}
