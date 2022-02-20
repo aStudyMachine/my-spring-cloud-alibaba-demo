ecf867c3263896b0a7db225e53d89d93aa305d2e
wukun <studymachine@foxmail.com>
2022-02-20 17:05:17 +0800

    6.5.4 【实例】将Spring Cloud Gateway应用接入Sentinel,管理流量控制规则

--- chapter-6/6-5-4-gateway-access-sentinel/src/main/java/com/alibaba/cloud/youxia/GatewayAccessSentinelApplication.java
+++ chapter-6/6-5-4-gateway-access-sentinel/src/main/java/com/alibaba/cloud/youxia/GatewayAccessSentinelApplication.java
@@ -3,9 +3,7 @@ package com.alibaba.cloud.youxia;
 import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.GatewayCallbackManager;
 import com.alibaba.csp.sentinel.init.InitExecutor;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
